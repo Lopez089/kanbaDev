08a8572eb0e38aa67bda2522f5ede96f48ca05e4
LopezDev <aragonlopezjuan87@gmail.com>
2024-01-24 06:35:29 +0100

    feat component card

---
+++ src/components/tag/tag.jsx
@@ -0,0 +1,14 @@
+import './tag.css'
+export const Tag = ({ children, color, bg }) => {
+  return (
+    <div
+      className="tag"
+      style={{
+        background: bg,
+        color: color
+      }}
+    >
+      {children}
+    </div>
+  )
+}
