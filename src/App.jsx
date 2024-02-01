150a56f76e0e34e6c6c4f772160b69c4982851bf
LopezDev <aragonlopezjuan87@gmail.com>
2024-02-01 06:53:34 +0100

    Container date

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,14 @@
 import { Card } from './components'
-import { ContainerAvatar } from './containers'
+import { ContainerAvatar, ContainerDate } from './containers'
 import './App.css'
 
 const App = () => {
-
+  const date = new Date
   return (
     <main>
       <section>
         <Card>
-          <ContainerAvatar
-            name={'juan lopez'}
-          />
-          <ContainerAvatar
-            name={'juan lopez Aragon'}
-          />
+          <ContainerDate date={date} />
           <ContainerAvatar
             name={'juan lopez'}
             avatar={'https://img.freepik.com/free-photo/portrait-handsome-hipster-man-glasses-3d-rendering_1142-51612.jpg?t=st=1706728585~exp=1706732185~hmac=2f5f8149e6d250163816c1d84d9c49e4823b96e0b9f24f4cc7b9eb1405d06f56&w=996'}
---
+++ src/containers/containerDate.jsx
@@ -0,0 +1,17 @@
+import PropTypes from "prop-types"
+
+export const ContainerDate = ({ date }) => {
+  const months = ['ENE', 'FEB', 'MAR', 'ABR', 'MAY', 'JUN', 'JUL', 'AGO', 'SEP', 'OCT', 'NOV', 'DIC']
+
+  const day = date.getDate()
+  const month = months[date.getMonth()]
+  const age = date.getFullYear().toString().slice(-2)
+
+  return (
+    <p>{`${day} ${month} ${age}`}</p>
+  )
+}
+
+ContainerDate.propTypes = {
+  date: PropTypes.instanceOf(Date)
+}
